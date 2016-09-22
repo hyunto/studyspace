c397b3c10f59759c81d9dd634c549134202ffb88
hyunto <eqelizer@naver.com>
2016-09-22 17:45:28 +0900

    목록 출력을 위한 신규 URL, 뷰, 템플릿

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(
+        r'^lists/the-only-list-in-the-world/$',
+        'lists.views.view_list',
+        name='view_lists'
+    )
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
