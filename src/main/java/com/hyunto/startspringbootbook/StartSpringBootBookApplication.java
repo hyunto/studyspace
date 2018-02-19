1003f85e735f6339b97fab642896ef784ffd87dd
hyunto <jhyunto@gmail.com>
2018-02-19 12:48:36 +0900

    Chapter 1.1.1 Spring Boot 프로젝트 생성

---
+++ src/main/java/com/hyunto/startspringbootbook/StartSpringBootBookApplication.java
@@ -0,0 +1,12 @@
+package com.hyunto.startspringbootbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringBootBookApplication.class, args);
+	}
+}
