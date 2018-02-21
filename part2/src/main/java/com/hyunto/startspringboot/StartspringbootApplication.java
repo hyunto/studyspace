ccec40793b7bf215553e97852faf4a06b62f82e6
hyunsoo0813 <hyunsoo0813@ncsoft.com>
2018-02-21 13:00:06 +0900

    프로젝트 구조 변경
    - Gradle 버전 변경 : 3.5.1 -> 4.5
    - 책의 Part 별로 Gradle Multi-Project 구성
    - Part2 프로젝트 생성

---
+++ part2/src/main/java/com/hyunto/startspringboot/StartspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.hyunto.startspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartspringbootApplication.class, args);
+	}
+}
