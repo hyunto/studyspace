b2793b2069d6fda014c7088841c4bbaa53d76ff2
hyunto <jhyunto@gmail.com>
2018-03-02 13:20:31 +0900

    Chapter 5 Thymeleaf 프로젝트 생성

---
+++ part2-thymeleaf/src/test/java/com/hyunto/startspringboot/part2thymeleaf/Part2ThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hyunto.startspringboot.part2thymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Part2ThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
