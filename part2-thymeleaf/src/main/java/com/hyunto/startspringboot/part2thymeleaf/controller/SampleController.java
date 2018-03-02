f2e61a5fed213286d79359e0b2402de6c132858c
hyunto <jhyunto@gmail.com>
2018-03-02 13:36:32 +0900

    Chapter 5.2 Thymeleaf 기본 동작 확인

---
+++ part2-thymeleaf/src/main/java/com/hyunto/startspringboot/part2thymeleaf/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.hyunto.startspringboot.part2thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+	@GetMapping("/sample1")
+	public void sample1(Model model) {
+//		model.addAttribute("greeting", "Hello World");
+		model.addAttribute("greeting", "안녕하세요");
+	}
+}
