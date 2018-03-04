9b244ce9aca71b7be6f14b0a6222aa247aa1b12a
hyunto <jhyunto@gmail.com>
2018-03-05 08:55:43 +0900

    Chapter 6.2.1 Repository 페이징 테스트
    - Query DSL을 사용한 페이징 처리 테스트
    - Query DSL을 사용한 검색 조건 처리 테스트

--- part3/src/main/java/com/hyunto/startspringboot/part3/persistence/WebBoardRepository.java
+++ part3/src/main/java/com/hyunto/startspringboot/part3/persistence/WebBoardRepository.java
@@ -1,9 +1,39 @@
 package com.hyunto.startspringboot.part3.persistence;
 
+import com.hyunto.startspringboot.part3.domain.QWebBoard;
 import com.hyunto.startspringboot.part3.domain.WebBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard> {
+
+    public default Predicate makePredicate(String type, String keyword) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        QWebBoard board = QWebBoard.webBoard;
+
+        // bno > 0
+        builder.and(board.bno.gt(0));
+
+        // type if ~ else
+        if (type == null) {
+            return builder;
+        }
+
+        switch (type) {
+            case "t":
+                builder.and(board.title.like("%" + keyword + "%"));
+                break;
+            case "c":
+                builder.and(board.content.like("%" + keyword + "%"));
+                break;
+            case "w":
+                builder.and(board.writer.like("%" + keyword + "%"));
+        }
+
+        return builder;
+    }
 
 }
--- part3/src/test/java/com/hyunto/startspringboot/part3/persistence/WebBoardRepositoryTests.java
+++ part3/src/test/java/com/hyunto/startspringboot/part3/persistence/WebBoardRepositoryTests.java
@@ -1,19 +1,23 @@
 package com.hyunto.startspringboot.part3.persistence;
 
 import com.hyunto.startspringboot.part3.domain.WebBoard;
-import groovy.util.logging.Log;
+import lombok.extern.java.Log;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.IntStream;
 
+@Log
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Log
 @Commit
 public class WebBoardRepositoryTests {
 
@@ -33,4 +37,33 @@ public class WebBoardRepositoryTests {
 			webBoardRepository.save(board);
 		});
 	}
+
+	@Test
+	public void testList1() {
+		Pageable pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "bno");
+
+        Page<WebBoard> result = webBoardRepository.findAll(webBoardRepository.makePredicate(null, null), pageable);
+
+        log.info("PAGE: " + result.getPageable());
+
+        log.info("-------------------------------------");
+
+        result.getContent().forEach(webBoard -> {
+            log.info("" + webBoard);
+        });
+	}
+
+	@Test
+    public void testList2() {
+	    Pageable pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "bno");
+
+	    Page<WebBoard> result = webBoardRepository.findAll(webBoardRepository.makePredicate("t", "10"), pageable);
+
+	    log.info("PAGE: " + result.getPageable());
+
+	    log.info("----------------------------------------");
+	    result.getContent().forEach(webBoard -> {
+	        log.info("" + webBoard);
+        });
+    }
 }
