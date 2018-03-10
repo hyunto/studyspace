514929606a808fb3cb35a8fe553177bd1bbc1db7
hyunto <jhyunto@gmail.com>
2018-03-11 01:28:25 +0900

    Chapter 8.3 단순 시큐리티 적용
    - Custom Login, Logout 페이지 및 기능 추가
    - Access Denied 페이지 생성

--- part4/src/main/java/com/hyunto/startspringboot/part4/config/SecurityConfig.java
+++ part4/src/main/java/com/hyunto/startspringboot/part4/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hyunto.startspringboot.part4.config;
 
 import lombok.extern.java.Log;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,5 +13,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("Security Config..........");
+
+		http.authorizeRequests().antMatchers("/guest/**").permitAll();
+
+		http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
+
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+
+		http.formLogin().loginPage("/login");
+
+		http.exceptionHandling().accessDeniedPage("/accessDenied");
+
+		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		log.info("build auth global.....");
+
+		auth.inMemoryAuthentication()
+				.withUser("manager")
+				.password("{noop}1111")
+				.roles("MANAGER");
 	}
 }
