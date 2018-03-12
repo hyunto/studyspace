96c8bfa2a0b5ba77a14988fa308719fcce3b5600
hyunto <jhyunto@gmail.com>
2018-03-12 23:25:30 +0900

    Chapter 8.6 Remember Me 인증
    - username, series, token 값을 데이터베이스에 저장하여 Remember Me 구현

--- part4/src/main/java/com/hyunto/startspringboot/part4/security/SecurityConfig.java
+++ part4/src/main/java/com/hyunto/startspringboot/part4/security/SecurityConfig.java
@@ -7,12 +7,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
 
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
 	@Autowired
     private HyuntoUserService userService;
@@ -21,25 +26,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("Security Config..........");
 
-		http.authorizeRequests().antMatchers("/guest/**").permitAll();
-
-		http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
-
-		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
-
-		http.formLogin().loginPage("/login");
-
-		http.exceptionHandling().accessDeniedPage("/accessDenied");
-
-		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
-
-		http.userDetailsService(userService);
+		http.authorizeRequests()
+                .antMatchers("/guest/**").permitAll()
+                .antMatchers("/manager/**").hasRole("MANAGER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .and()
+            .exceptionHandling()
+                .accessDeniedPage("/accessDenied")
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .and()
+            .rememberMe()
+                .key("hyunto")
+                .userDetailsService(userService)
+                .tokenRepository(this.getJDBCRepository())
+                .tokenValiditySeconds(60*60*24);
 	}
 
-	/*
-	@Autowired
-	private DataSource dataSource;
+	private PersistentTokenRepository getJDBCRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
 
+	/*
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		log.info("build auth global.....");
