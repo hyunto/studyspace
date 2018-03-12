11b8157058a8434aa42ba207c3bdd2e5e7aedde0
hyunto <jhyunto@gmail.com>
2018-03-12 13:30:48 +0900

    Chapter 8.4 DB를 사용한 인증방식
    - JDBC를 사용한 인증방식
    - Custom UserDetailsService 구현

--- part4/src/main/java/com/hyunto/startspringboot/part4/security/SecurityConfig.java
+++ part4/src/main/java/com/hyunto/startspringboot/part4/security/SecurityConfig.java
@@ -1,14 +1,21 @@
-package com.hyunto.startspringboot.part4.config;
+package com.hyunto.startspringboot.part4.security;
 
+import com.hyunto.startspringboot.part4.security.HyuntoUserService;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+    private HyuntoUserService userService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -25,7 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.exceptionHandling().accessDeniedPage("/accessDenied");
 
 		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+
+		http.userDetailsService(userService);
 	}
+
+	/*
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -35,5 +48,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.withUser("manager")
 				.password("{noop}1111")
 				.roles("MANAGER");
+
+		String query1 = "SELECT uid username, CONCAT('{noop}', upw) password, true enabled FROM tbl_members WHERE uid = ?";
+		String query2 = "SELECT member uid, role_name role FROM tbl_member_roles WHERE member = ?";
+
+		auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(query1)
+                .rolePrefix("ROLE_")
+                .authoritiesByUsernameQuery(query2);
 	}
+	*/
 }
