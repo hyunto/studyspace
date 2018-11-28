fcd3699c69325a27123bf98c6a685b1368dd457a
jhyunto <jhyunto@gmail.com>
2018-11-28 13:48:42 +0900

    Gradle 멀티 프로젝트 구성 및 simplemvc 프로젝트 추가

---
+++ simplemvc/src/main/java/xyz/hyunto/simplemvc/SimplemvcApplication.java
@@ -0,0 +1,12 @@
+package xyz.hyunto.simplemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemvcApplication.class, args);
+	}
+}
