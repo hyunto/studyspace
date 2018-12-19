3581bbd85eda79ea8e61b83ef224c27ad2e769fb
hyunto <jhyunto@gmail.com>
2018-12-20 00:55:04 +0900

    02장. 클라이언트 정보를 저장하기 위한 관계형 데이터베이스 이용 진행 중
    - oauth_approvals 테이블 생성 중 'Invalid default values for...' 에러 발생

---
+++ rdbms-server/src/main/java/xyz/hyunto/rdbmsserver/RdbmsServerApplication.java
@@ -0,0 +1,14 @@
+package xyz.hyunto.rdbmsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdbmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdbmsServerApplication.class, args);
+	}
+
+}
+
