2164890cc3945b21c8af1c861a7a2044cc13dd06
jhyunto <jhyunto@gmail.com>
2018-12-26 19:10:03 +0900

    02장. 클라이언트 등록 구현

---
+++ oauth2provider/src/main/java/xyz/hyunto/oauth2provider/api/UserController.java
@@ -0,0 +1,23 @@
+package xyz.hyunto.oauth2provider.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/api/profile")
+    public ResponseEntity<UserProfile> profile() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = user.getUsername() + "@mailinator.com";
+
+        UserProfile profile = new UserProfile();
+        profile.setName(user.getUsername());
+        profile.setEmail(email);
+
+        return ResponseEntity.ok(profile);
+    }
+}
