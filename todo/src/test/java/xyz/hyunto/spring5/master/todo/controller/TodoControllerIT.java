21bd8191d536ae10b663667f87bb6e21c6b23bed
hyunto <jhyunto@gmail.com>
2019-07-18 00:33:05 +0900

    06. 스프링 시큐리티로 REST 서비스 보호 실습 완료
    - Basic 인증 완료
    - OAuth2 실습 Skip.

--- todo/src/test/java/xyz/hyunto/spring5/master/todo/controller/TodoControllerIT.java
+++ todo/src/test/java/xyz/hyunto/spring5/master/todo/controller/TodoControllerIT.java
@@ -6,12 +6,17 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import xyz.hyunto.spring5.master.todo.TodoApplication;
 import xyz.hyunto.spring5.master.todo.model.Todo;
 
 import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.Date;
 
 import static org.hamcrest.Matchers.containsString;
@@ -28,9 +33,21 @@ public class TodoControllerIT {
     private int port;
 
     private TestRestTemplate template = new TestRestTemplate();
+    private HttpHeaders headers = createHeaders("user", "P@ssw0rd");
 
     private String createUrl(String uri) {
         return "http://localhost:" + port + uri;
+    }
+
+    private HttpHeaders createHeaders(String username, String password) {
+        return new HttpHeaders() {
+            {
+                String auth = username + ":" + password;
+                byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+                String authHeader = "Basic " + new String(encodedAuth);
+                set("Authorization", authHeader);
+            }
+        };
     }
 
     @Test
@@ -38,7 +55,9 @@ public class TodoControllerIT {
         String expected = "[" +
                 "{id:1,user:Jack,desc:\"Learn Spring MVC\",done:false}" + "," +
                 "{id:2,user:Jack,desc:\"Learn Struts\", done:false}" + "]";
-        ResponseEntity<String> response = template.getForEntity(createUrl("/users/Jack/todos"), String.class);
+//        ResponseEntity<String> response = template.getForEntity(createUrl("/users/Jack/todos"), String.class);
+        ResponseEntity<String> response = template.exchange(createUrl("users/Jack/todos"), HttpMethod.GET,
+                new HttpEntity<>(null, headers), String.class);
         JSONAssert.assertEquals(expected, response.getBody(), false);
     }
 
