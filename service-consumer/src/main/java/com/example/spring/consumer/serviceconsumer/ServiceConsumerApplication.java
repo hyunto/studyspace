93493670e1df51db70ebb0a36a5f56cb651708ec
hyunto <jhyunto@gmail.com>
2019-07-25 00:05:28 +0900

    09. 스프링 클라우드 : Eureka를 통한 Service Discovery 구현

--- service-consumer/src/main/java/com/example/spring/consumer/serviceconsumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/example/spring/consumer/serviceconsumer/ServiceConsumerApplication.java
@@ -2,9 +2,11 @@ package com.example.spring.consumer.serviceconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients("com.example.spring.consumer.serviceconsumer")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceConsumerApplication {
 
