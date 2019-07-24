14741a09e65f308f44164b60fc39b55c5b37edd0
hyunto <jhyunto@gmail.com>
2019-07-24 22:42:01 +0900

    09. 스프링 클라우드 : 스프링 클라우드 버스 실습 완료
    - 스프링 클라우드 버스를 통해 컨피그 변경사항을 다중 인스턴스로 동시에 전파하도록 구조 개선
      - 단일 인스턴스 설정 리프레시 : /actuator/refresh
      - 멀티 인스턴스 설정 리프레시 : /actuator/bus-refresh

--- microservice-a/src/main/java/com/example/spring/cloud/microservicea/controller/MessageController.java
+++ microservice-a/src/main/java/com/example/spring/cloud/microservicea/controller/MessageController.java
@@ -2,12 +2,14 @@ package com.example.spring.cloud.microservicea.controller;
 
 import com.example.spring.cloud.microservicea.property.ApplicationConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 public class MessageController {
 
