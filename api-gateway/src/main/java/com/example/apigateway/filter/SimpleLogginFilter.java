a1520afb130bf42100915418e91d8c949421f84e
hyunto <jhyunto@gmail.com>
2019-07-26 13:33:17 +0900

    09. 스프링 클라우드 : Zuul Filter 추가

---
+++ api-gateway/src/main/java/com/example/apigateway/filter/SimpleLogginFilter.java
@@ -0,0 +1,37 @@
+package com.example.apigateway.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class SimpleLogginFilter extends ZuulFilter {
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest httpRequest = context.getRequest();
+		System.out.println(String.format("Request Method : %s n URL: %S", httpRequest.getMethod(), httpRequest.getRequestURL().toString()));
+		return null;
+	}
+
+}
