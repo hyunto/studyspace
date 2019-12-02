039a75751ff098054f68964f4dec793d68d35cb7
hyunto <jhyunto@gmail.com>
2019-12-02 14:22:35 +0900

    gulp로 uglify 적용

--- javascript/learning_javascript_book/gulpfile.js
+++ javascript/learning_javascript_book/gulpfile.js
@@ -3,6 +3,8 @@ const {series, parallel} = require('gulp');
 const {src, dest} = require('gulp');
 const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 function esLint() {
   return src('es6/**/*.js', 'public/es6/**/*.js')
@@ -13,12 +15,18 @@ function esLint() {
 function nodeBabel() {
   return src('es6/**/*.js')
       .pipe(babel())
+      .pipe(dest('dist'))
+      .pipe(uglify())
+      .pipe(rename({extname: '.min.js'}))
       .pipe(dest('dist'));
 }
 
 function browserBabel() {
   return src('public/es6/**/*.js')
       .pipe(babel())
+      .pipe(dest('public/dist'))
+      .pipe(uglify())
+      .pipe(rename({extname: '.min.js'}))
       .pipe(dest('public/dist'));
 }
 
