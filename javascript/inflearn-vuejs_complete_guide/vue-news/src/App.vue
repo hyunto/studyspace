77b5b474d2e5c75a620c60ae099635df1afe91ef
hyunto <jhyunto@gmail.com>
2019-12-21 22:25:02 +0900

    Vue.js 완벽 가이드 - vue-router 설치 및 구성

--- javascript/inflearn-vuejs_complete_guide/vue-news/src/App.vue
+++ javascript/inflearn-vuejs_complete_guide/vue-news/src/App.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    hello
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  // name: 'app',
-  // components: {
-  //   HelloWorld
-  // }
-  methods: {
-    fetchData() {
-      console.log('hello');
-    }
-  }
+
 }
 </script>
 
