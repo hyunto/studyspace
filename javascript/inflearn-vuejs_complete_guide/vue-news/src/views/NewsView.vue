49d2e6edcc3301d384f122975d020e97c18d1ffd
hyunto <jhyunto@gmail.com>
2019-12-23 23:33:51 +0900

    Vue.js 완벽 가이드 : Vuex 적용

--- javascript/inflearn-vuejs_complete_guide/vue-news/src/views/NewsView.vue
+++ javascript/inflearn-vuejs_complete_guide/vue-news/src/views/NewsView.vue
@@ -1,29 +1,16 @@
 <template>
   <div>
     news
-    <div v-for="user in users">{{ user }}</div>
+    <div v-for="user in this.$store.state.news">{{ user }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
+
 
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    var _this = this;
-    fetchNewsList()
-      .then(function(response) {
-        console.log(response);
-        _this.users = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    this.$store.dispatch('FETCH_NEWS');
   }
 }
 </script>
