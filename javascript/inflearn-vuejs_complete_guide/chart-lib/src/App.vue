25923ba49800169b52af206736dae2b9d1da3f46
hyunto <jhyunto@gmail.com>
2020-01-09 22:37:14 +0900

    Vue.js 완벽 가이드: chart.js의 LineChart 추가 및 컴포넌트화

--- javascript/inflearn-vuejs_complete_guide/chart-lib/src/App.vue
+++ javascript/inflearn-vuejs_complete_guide/chart-lib/src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <h1>Chart.js</h1>
     <bar-chart></bar-chart>
+    <line-chart></line-chart>
   </div>
 </template>
 
 <script>
 import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
 
 export default {
   components: {
-    BarChart
+    BarChart,
+    LineChart
   }  
 }
 </script>
