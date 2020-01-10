70400d9e69d2557170fb3d3e2d81471f2a140c03
hyunto <jhyunto@gmail.com>
2020-01-10 23:34:14 +0900

    Vue.js 완벽 가이드: 컴포넌트 디자인 패턴 - Controlled (문제점 존재, 해결필요)

---
+++ javascript/inflearn-vuejs_complete_guide/component-design-patterns/controlled/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <check-box :checked="checked"></check-box>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox';
+
+export default {
+  components: {
+    CheckBox,
+  },
+  data() {
+    return {
+      checked: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
