b911445b986f3a58133f177b0001317b998a8d6a
hyunto <jhyunto@gmail.com>
2020-01-10 22:51:18 +0900

    Vue.js 완벽 가이드: 컴포넌트 디자인 패턴 - Common

---
+++ javascript/inflearn-vuejs_complete_guide/component-design-patterns/common/src/components/AppContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+    <!-- 핵심내용 Start -->
+    <button @click="$emit('renew')">renew items</button>
+    <!-- 핵심내용 End -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
