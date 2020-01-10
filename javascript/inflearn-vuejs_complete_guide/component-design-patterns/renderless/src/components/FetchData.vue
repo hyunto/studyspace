90bf0ef48df1177361da91f7ecea6f55fc524fe9
hyunto <jhyunto@gmail.com>
2020-01-11 00:43:16 +0900

    Vue.js 완벽 가이드: 컴포넌트 디자인 패턴 - Renderless

---
+++ javascript/inflearn-vuejs_complete_guide/component-design-patterns/renderless/src/components/FetchData.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export default {
+  props: {
+    url: String
+  },
+  data() {
+    return {
+      response: null,
+      loading: true,
+    }
+  },
+  created() {
+    axios.get(this.url)
+      .then(response => {
+        this.response = response.data;
+        this.loading = false;
+      })
+      .catch(error => {
+        alert('[ERROR] fetching the data', error);
+      })
+  },
+  render() {
+    return this.$scopedSlots.default({
+      response: this.response,
+      loading: this.loading
+    })
+  }
+}
+</script>
