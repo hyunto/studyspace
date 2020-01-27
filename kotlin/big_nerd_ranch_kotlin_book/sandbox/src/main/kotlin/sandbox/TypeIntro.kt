96eeedbd2bdbfdb20ca1fe452f7cb603c1062d3d
hyunto <jhyunto@gmail.com>
2020-01-28 01:03:42 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 2장. 타입추론 : 완료

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/TypeIntro.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/TypeIntro.kt
@@ -1,6 +1,13 @@
 package sandbox
 
 fun main(args: Array<String>) {
-	var experiencePoints: Int = 5
+	val playerName = "현토"
+// 	playerName = "hyunto"	// 컴파일 에러 : Val cannot be reassigned
+
+	var experiencePoints = 5
+//	var experiencePoints: Int = "thirty-two"	// Type mismatch 컴파일 에러
+	experiencePoints += 5;
+
 	println(experiencePoints)
+	println(playerName)
 }
