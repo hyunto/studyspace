1c7cbc02642f848f6a7615dd284893d4e8e91353
hyunto <jhyunto@gmail.com>
2020-01-28 23:55:23 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 2장. 변수, 상수, 타입 : 완료

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/TypeIntro.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/TypeIntro.kt
@@ -1,6 +1,10 @@
 package sandbox
 
+const val MAX_EXPERIENCE: Int = 5000
+
 fun main(args: Array<String>) {
+//	MAX_EXPERIENCE = 10000	// 컴파일 에러 : Val cannot be reassigned
+
 	val playerName = "현토"
 // 	playerName = "hyunto"	// 컴파일 에러 : Val cannot be reassigned
 
@@ -8,6 +12,21 @@ fun main(args: Array<String>) {
 //	var experiencePoints: Int = "thirty-two"	// Type mismatch 컴파일 에러
 	experiencePoints += 5;
 
-	println(experiencePoints)
+	var hasSteed = false
+	var coin = 0
+
+	val barName = "유니콘의 뿔"
+	val barOwnerName = "마르티나"
+	val barMenu = mapOf<String, Int>("beer" to 3500, "soju" to 3000)
+
 	println(playerName)
+	println(coin)
+	println(hasSteed)
+	println(experiencePoints)
+	println()
+	println(barName)
+	println(barOwnerName)
+	barMenu.forEach { (k, v) -> println("[Menu] $k : $v") }
+	println()
+	println(playerName.reversed())
 }
