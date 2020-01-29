2223033464317f5a02f8d3a866adf161e7234a9d
hyunto <jhyunto@gmail.com>
2020-01-30 00:12:52 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 3장. 조건문과 조건식 - when 표현식으로 조건문을 변환

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Game.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Game.kt
@@ -13,20 +13,16 @@ fun main(args: Array<String>) {
 	val auraColor = if(auraVisible) "GREEN" else "NONE"
 	println(auraColor)
 
-	val healthStatus = if (healthPoints == 100) {
-		"최상의 상태임!"
-	} else if (healthPoints in 90..99){
-		"약간의 찰과상만 있음"
-	} else if (healthPoints in 75..89) {
-		if (isBlessed) {
-			"경미한 상처가 있지만 빨리 치유됨!"
-		} else {
-			"경미한 상처만 있음"
-		}
-	} else if (healthPoints in 15..74) {
-		"많이 다친 것 같음"
-	} else {
-		"최악의 상태임!"
+	val healthStatus = when(healthPoints) {
+		100 -> "최상의 상태임!"
+		in 90..99 -> "약간의 찰과상만 있음"
+		in 75..89 -> if (isBlessed) {
+						"경미한 상처가 있지만 빨리 치유됨!"
+					} else {
+						"경미한 상처만 있음"
+					}
+		in 15..74 -> "많이 다친 것 같음"
+		else -> "최악의 상태임!"
 	}
 
 	// 플레이어의 상태 출력
