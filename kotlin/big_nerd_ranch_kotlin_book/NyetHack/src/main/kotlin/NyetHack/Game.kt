669ed2a73e7accb8677c0fa2b25bf3ac94c25211
hyunto <jhyunto@gmail.com>
2020-01-30 23:48:38 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 4장. 함수 : 함수를 사용하여 코드 리팩토링(extract)

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Game.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Game.kt
@@ -9,22 +9,36 @@ fun main(args: Array<String>) {
 	val isImmortal = false
 
 	// 아우라
-	val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-	val auraColor = if(auraVisible) "GREEN" else "NONE"
+	val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+	val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
-	val healthStatus = when(healthPoints) {
+	// 플레이어의 상태 출력
+	printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+	println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
+	println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+	val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+	val auraColor = if (auraVisible) "GREEN" else "NONE"
+	return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+	val healthStatus = when (healthPoints) {
 		100 -> "최상의 상태임!"
 		in 90..99 -> "약간의 찰과상만 있음"
 		in 75..89 -> if (isBlessed) {
-						"경미한 상처가 있지만 빨리 치유됨!"
-					} else {
-						"경미한 상처만 있음"
-					}
+			"경미한 상처가 있지만 빨리 치유됨!"
+		} else {
+			"경미한 상처만 있음"
+		}
 		in 15..74 -> "많이 다친 것 같음"
-		else -> "최악의 상태임!"
+		in 0..14 -> "최악의 상태임!"
+		else -> "error"
 	}
-
-	// 플레이어의 상태 출력
-	println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
-	println("$name $healthStatus")
+	return healthStatus
 }
