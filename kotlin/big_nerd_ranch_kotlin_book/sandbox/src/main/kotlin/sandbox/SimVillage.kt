9ffec5fd89f1a4741f8eebb266ce0cd4c72c5ef6
hyunto <jhyunto@gmail.com>
2020-01-31 23:19:38 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 5장. 익명함수와 함수타입 - 익명함수에 함수타입 지정

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
@@ -1,8 +1,9 @@
 package sandbox
 
 fun main(args: Array<String>) {
-	println({
+	val greetingFunction: () -> String = {
 		val currentYear = 2020
 		"SimVillage 방문을 환영합니다, 촌장님! (copyright $currentYear)"
-	} ())
+	}
+	println(greetingFunction())
 }
