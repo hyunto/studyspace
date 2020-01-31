5f69394f9b3c6b9d762e9d3ae45dcc5ea95e556e
hyunto <jhyunto@gmail.com>
2020-01-31 23:20:25 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 5장. 익명함수와 함수타입 - it 키워드 사용

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
@@ -1,9 +1,9 @@
 package sandbox
 
 fun main(args: Array<String>) {
-	val greetingFunction: (String) -> String = { playerName ->
+	val greetingFunction: (String) -> String = {
 		val currentYear = 2020
-		"SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+		"SimVillage 방문을 환영합니다, $it 님! (copyright $currentYear)"
 	}
 	println(greetingFunction("hyunto"))
 }
