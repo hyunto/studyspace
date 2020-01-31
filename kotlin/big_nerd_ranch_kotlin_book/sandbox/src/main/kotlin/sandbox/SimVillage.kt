74b4a71539cd1a753673be68d33e4c51705564db
hyunto <jhyunto@gmail.com>
2020-01-31 23:20:50 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 5장. 익명함수와 함수타입 - 다수의 함수 매개변수 사용

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
@@ -1,9 +1,10 @@
 package sandbox
 
 fun main(args: Array<String>) {
-	val greetingFunction: (String) -> String = {
+	val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
 		val currentYear = 2020
-		"SimVillage 방문을 환영합니다, $it 님! (copyright $currentYear)"
+		println("$numBuildings 채의 건물이 추가된")
+		"SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
 	}
-	println(greetingFunction("hyunto"))
+	println(greetingFunction("hyunto", 2))
 }
