c41f3215c55cdcb115b502abfadf1b3c186ed1e9
hyunto <jhyunto@gmail.com>
2020-02-03 00:01:18 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 5장. 익명함수와 함수타입 - 함수를 반환하는 함수 + 클로저

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/SimVillage.kt
@@ -67,7 +67,12 @@ fun main(args: Array<String>) {
 		println("$numBuildings 채의 건물이 추가됨")
 		"SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
 	}
+	println()
 
+	// --------------------------------------------------------------------
+	// 함수를 반환하는 함수
+	println("*** 함수를 반환하는 함수 ***")
+	runSimulationReteruningFunction()
 }
 
 fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
@@ -86,7 +91,23 @@ inline fun runSimulationFunctionReference(playerName: String, costPrinter: (Int)
 	println(greetingFunction(playerName, numBuildings))
 }
 
+fun runSimulationReteruningFunction() {
+	val greetingFunction = configureGreetingFunction()
+	println(greetingFunction("hyunto"))
+}
+
 fun printConstructionCost(numBuildings: Int) {
 	val cost = 500
 	println("건축 비용: ${cost * numBuildings}")
+}
+
+fun configureGreetingFunction(): (String) -> String {
+	val structureType = "병원"
+	var numBuildings = 5
+	return { playerName: String ->
+		val currentYear = 2020
+		numBuildings += 1
+		println("$numBuildings 채의 $structureType 이 추가됨")
+		"SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+	}
 }
