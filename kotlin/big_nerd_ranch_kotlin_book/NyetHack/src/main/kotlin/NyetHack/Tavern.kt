d214ac149ed3c16861cc0b9a9dfb6d5316bd6819
hyunto <jhyunto@gmail.com>
2020-02-03 22:08:27 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 5장. null 안전과 예외 - Safe Call Operator with let

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Tavern.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Tavern.kt
@@ -1,7 +1,13 @@
 package NyetHack
 
 fun main(args: Array<String>) {
-	var beverage = readLine()?.capitalize()
+	var beverage = readLine()?.let {
+		if (it.isNotBlank()) {
+			it.capitalize()
+		} else {
+			"맥주"
+		}
+	}
 //	beverage = null
 	println(beverage)
 }
