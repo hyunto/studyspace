717e503594bb48789c84610e1ca8d02077437c84
hyunto <jhyunto@gmail.com>
2020-02-03 22:48:07 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 5장. null 안전과 예외 - 안전한 nullable 값 처리 방법 정리
    1. Safe Call Operator (?.)
    2. Safe Call Operator with let (?.let{})
    3. Non-Null Assertion Operator (!!)
    4. if(variable != null)
    5. Null Coalescing Operator (?:)

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Tavern.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Tavern.kt
@@ -1,13 +1,34 @@
 package NyetHack
 
 fun main(args: Array<String>) {
-	var beverage = readLine()?.let {
+	//Safe Call Operator
+	var nullSafe1 = readLine()?.capitalize()
+	println(nullSafe1)
+
+	// Safe Call Operator with let
+	var nullSafe2 = readLine()?.let{
 		if (it.isNotBlank()) {
 			it.capitalize()
 		} else {
-			"맥주"
+			"empty space"
 		}
 	}
-//	beverage = null
-	println(beverage)
+	println(nullSafe2)
+
+	// Non-Null Assertion Operation
+	var nullSafe3 = readLine()!!.capitalize()
+	println(nullSafe3)
+
+	// if..else
+	var nullSafe4 = readLine()
+	if (nullSafe4 != null) {
+		nullSafe4 = nullSafe3.capitalize()
+	} else {
+		println("[Error] input value is null.")
+	}
+	println(nullSafe4)
+
+	// Null Coalescing Operator
+	var nullSafe5 = readLine() ?: "empty space"
+	println(nullSafe5)
 }
