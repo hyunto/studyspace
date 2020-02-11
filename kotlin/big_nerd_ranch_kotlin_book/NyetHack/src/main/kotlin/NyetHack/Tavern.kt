4a5d98b5ab1b7c5b656d38304914770658e5dc5d
hyunto <jhyunto@gmail.com>
2020-02-11 14:58:54 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 7장. 문자열 - 문자열 변경하기

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Tavern.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Tavern.kt
@@ -52,16 +52,27 @@ fun main(args: Array<String>) {
 	}
 }
 
+private fun toDragonSpeak(phrase: String) =
+	phrase.replace(Regex("[aeiou]")) {
+		when(it.value) {
+			"a" -> "4"
+			"e" -> "3"
+			"i" -> "1"
+			"o" -> "0"
+			"u" -> "|_|"
+			else -> it.value
+		}
+	}
+
 private fun placeOrder(menuData: String) {
 	val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
 	val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
 	println("마드리갈은 $tavernMaster 에게 주문한다.")
 
-//	val data = menuData.split(',')
-//	val type = data[0]
-//	val name = data[1]
-//	val price = data[2]
 	val (type, name, price) = menuData.split(',')
 	val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
 	println(message)
+
+	val phrase = "와, $name 진짜 좋구나!"
+	println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
 }
