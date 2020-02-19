d201c67cf62387e53eb9fcd12214c0580bd380f9
hyunto <jhyunto@gmail.com>
2020-02-20 00:01:26 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 15장. 객체 - enum 클래스

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Coordinate.kt
+++
@@ -1,5 +0,0 @@
-package NyetHack
-
-data class Coordinate(val x: Int, val y: Int) {
-	val isInBounds = x >= 0 && y >= 0
-}
--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Navigation.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Navigation.kt
@@ -1,4 +1,24 @@
 package NyetHack
 
-class Navigation {
+enum class Direction(private val coordinate: Coordinate) {
+	NORTH(Coordinate(0, -1)),
+	EAST(Coordinate(1, 0)),
+	SOUTH(Coordinate(0, 1)),
+	WEST(Coordinate(-1, 0));
+
+	fun updateCoordinate(playerCoordinate: Coordinate) =
+		Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
+
 }
+
+data class Coordinate(val x: Int, val y: Int) {
+	val isInBounds = x >= 0 && y >= 0
+}
+
+fun getDirectionName(direction: Direction) =
+	when (direction) {
+		Direction.NORTH -> "북"
+		Direction.EAST -> "동"
+		Direction.SOUTH -> "남"
+		Direction.WEST -> "서"
+	}
