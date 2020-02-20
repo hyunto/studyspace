093a646ba8f2adb1ba6d3d2780fb9581793a7299
hyunto <jhyunto@gmail.com>
2020-02-20 13:44:35 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 15장. 객체 - 연산자 오버로딩

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Navigation.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Navigation.kt
@@ -6,13 +6,14 @@ enum class Direction(private val coordinate: Coordinate) {
 	SOUTH(Coordinate(0, 1)),
 	WEST(Coordinate(-1, 0));
 
-	fun updateCoordinate(playerCoordinate: Coordinate) =
-		Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
+	fun updateCoordinate(playerCoordinate: Coordinate) = coordinate + playerCoordinate
 
 }
 
 data class Coordinate(val x: Int, val y: Int) {
 	val isInBounds = x >= 0 && y >= 0
+
+	operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
 
 fun getDirectionName(direction: Direction) =
