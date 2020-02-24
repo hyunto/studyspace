c3a1bfef7e74d09ad4db7982b0be5fe831b4c493
hyunto <jhyunto@gmail.com>
2020-02-24 23:52:17 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 16장. 인터페이스와 추상클래스 - 추상 클래스 구현

--- kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Creature.kt
+++ kotlin/big_nerd_ranch_kotlin_book/NyetHack/src/main/kotlin/NyetHack/Creature.kt
@@ -13,3 +13,20 @@ interface Fightable {
 
 	fun attack(opponent: Fightable): Int
 }
+
+abstract class Monster(val name: String,
+					   val description: String,
+					   override var healthPoints: Int) : Fightable {
+	override fun attack(opponent: Fightable): Int {
+		val damageDealth = damageRoll
+		opponent.healthPoints -= damageDealth
+		return damageDealth
+	}
+}
+
+class Goblin(name: String = "Goblin",
+			 description: String = "추하게 생긴 고블린",
+			 healthPoints: Int = 30) : Monster(name, description, healthPoints) {
+	override val diceCount = 2
+	override val diceSides = 8
+}
