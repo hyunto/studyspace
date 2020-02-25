f3c86edaa4ef95f7c427680b791649aa13d57bc8
hyunto <jhyunto@gmail.com>
2020-02-25 23:14:38 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 17장. 제네릭 - 복합 제네릭 타입 매개변수, 타입 제약, vararg

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/Generics.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/Generics.kt
@@ -1,24 +1,46 @@
 package sandbox
 
-class LootBox<T>(item: T) {
+class LootBox<T : Loot>(vararg item: T) {    // 타입 제약 (T : Loot)
 	var open = false
-	private var loot: T = item
+	private var loot: Array<out T> = item
 
-	fun fetch(): T? {
-		return loot.takeIf { open }
+	// 인덱스 연산자 오버로딩으로 제네릭 타입 매개변수 배열 읽기
+	operator fun get(index: Int): T? = loot[index].takeIf { open }
+
+	fun fetch(item: Int): T? {
+		return loot[item].takeIf { open }
+	}
+
+	// 복합 제네릭 타입 매개변수
+	fun <R> fetch(item: Int, lootModFunction: (T) -> R): R? {
+		return lootModFunction(loot[item]).takeIf { open }
 	}
 }
 
-class Fedora(val name: String, val value: Int)
+open class Loot(val value: Int)
 
-class Coin(val value: Int)
+class Fedora(val name: String, value: Int) : Loot(value)
+
+class Coin(value: Int) : Loot(value)
 
 fun main(args: Array<String>) {
-	val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("평범한 중절모", 15))
+	val lootBoxOne: LootBox<Fedora> = LootBox(
+		Fedora("평범한 중절모", 15),
+		Fedora("눈부신 자주색 중절모", 25)
+	)
 	val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 
 	lootBoxOne.open = true
-	lootBoxOne.fetch()?.run {
+	lootBoxOne.fetch(1)?.run {
 		println("$name 를 LootBox에서 꺼냈습니다!")
 	}
+
+	// 복합 제네릭 타입 매개변수
+	val coin = lootBoxOne.fetch(0) {
+		Coin(it.value * 3)
+	}
+	coin?.let { println(it.value) }
+
+	val fedora = lootBoxOne[1]
+	fedora?.let { println(it.name) }
 }
