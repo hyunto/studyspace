86c3b22784d3cf80a1e893d0823600009b49d9f8
hyunto <jhyunto@gmail.com>
2020-02-26 21:51:04 +0900

    [빅 너드 랜치의 코틀린 프로그래밍] 18장. 확장 - 제네릭 확장 함수

--- kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/Extensions.kt
+++ kotlin/big_nerd_ranch_kotlin_book/sandbox/src/main/kotlin/sandbox/Extensions.kt
@@ -2,9 +2,11 @@ package sandbox
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+fun <T> T.easyPrint(): T{
+	println(this)
+	return this
+}
 
 fun main(args: Array<String>) {
-	"Hello World".addEnthusiasm(3).easyPrint()    // Hello World!!! 출력
-	19.easyPrint()	// 19 출력
+	"Hello World".easyPrint().addEnthusiasm(3).easyPrint()    // Hello World!!! 출력
 }
