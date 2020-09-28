0e8ce6477874aed03180554cd6fbb87e601f8e34
hyunto <jhyunto@gmail.com>
2020-09-29 00:10:09 +0900

    [Do it! 리액트 프로그래밍 책] 03-2. 컴포넌트와 구성 요소 실습

--- javascript/doit_react_programming/do-it-example/src/App.js
+++ javascript/doit_react_programming/do-it-example/src/App.js
@@ -3,12 +3,14 @@ import React, {
 } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodaysPlan from './03/TodaysPlan';
 
 class App extends Component {
   render() {
-    return ( 
+    return (
       <div className="app">
-        <h1 className="title">두잇!리액트 시작하기</h1> 
+        <h1 className="title">두잇!리액트 시작하기</h1>
+        <TodaysPlan/>
       </div>
     );
   }
