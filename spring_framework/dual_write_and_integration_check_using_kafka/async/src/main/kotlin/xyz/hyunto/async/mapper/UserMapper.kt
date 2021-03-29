5f4964a2d9f035e5864f2daf143314d0aff502bd
hyunto <jhyunto@gmail.com>
2021-03-29 11:47:07 +0900

    [DualWriteConsistency] 양쪽읽기 후 데이터 비교하는 컨슈머 로직 수정

--- spring_framework/dual_write_and_integration_check_using_kafka/async/src/main/kotlin/xyz/hyunto/async/mapper/UserMapper.kt
+++ spring_framework/dual_write_and_integration_check_using_kafka/async/src/main/kotlin/xyz/hyunto/async/mapper/UserMapper.kt
@@ -1,6 +1,8 @@
 package xyz.hyunto.async.mapper
 
 import org.apache.ibatis.annotations.Param
+import org.apache.ibatis.annotations.Result
+import org.apache.ibatis.annotations.Results
 import org.apache.ibatis.annotations.Select
 import org.springframework.stereotype.Repository
 import xyz.hyunto.async.model.User
@@ -30,6 +32,11 @@ interface UserMapper {
 	""")
 	fun selectByName(name: String): User?
 
+	@Results(id = "result", value = [
+		@Result(column = "id", property = "id"),
+		@Result(column = "name", property = "name"),
+		@Result(column = "age", property = "age")
+	])
 	@Select("""
 		SELECT 
 			id,
