fbde80256fb98ef57265cca110b8dfa5aa1e547e
hyunto <jhyunto@gmail.com>
2021-05-01 00:16:24 +0900

    [Kotlin Coroutine] 테스트용 프로젝트 추가

---
+++ kotlin/coroutine_book/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "xyz.hyunto"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Kotlin Coroutine (https://github.com/Kotlin/kotlinx.coroutines)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
