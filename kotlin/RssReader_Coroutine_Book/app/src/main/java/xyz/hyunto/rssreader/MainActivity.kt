ebd4dc71237207d65c94576ab60bbcc19a3126f2
hyunto <jhyunto@gmail.com>
2021-05-11 00:56:25 +0900

    [코틀린 동시성 프로그래밍] 2장. 안드로이드 RssReader 프로젝트 생성
    - 비동기 호출 함수 개선 방법 3가지

--- kotlin/RssReader_Coroutine_Book/app/src/main/java/xyz/hyunto/rssreader/MainActivity.kt
+++ kotlin/RssReader_Coroutine_Book/app/src/main/java/xyz/hyunto/rssreader/MainActivity.kt
@@ -3,10 +3,7 @@ package xyz.hyunto.rssreader
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.*
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
@@ -18,19 +15,55 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 개선방법 1) 비동기 호출자로 감싼 동기 함수
+        // loadNews()를 launch 블록으로 감싸지 않으면 UI
+        // 스레드에서 네트워크 요청이 일어나기 때문에 NetworksOnMainThreadException 이 발생한다.
         GlobalScope.launch(netDispatcher) {
-            val headlines = fetchRssHeadlines()
-            val newsCount = findViewById<TextView>(R.id.newsCount)
-            GlobalScope.launch(Dispatchers.Main) {
-                newsCount.text = "Found ${headlines.size} News"
-            }
+            loadNews()
         }
+
+        // 개선방법 2) 미리 정의된 디스패처를 갖는 비동기 함수
+        // 코드는 간단해지지만 백그라운드 스레드(netDispatcher)에서 강제로 실행되기 때문에 유연성이 떨어진다
+        // 추가로 함수 이름에서 비동기로 실행됨을 잘 나타내야 한다
+        asyncLoadNews()
+
+        // 개선방법 3) 유연한 디스패처를 가지는 비동기 함수
+        asyncLoadNews(netDispatcher)
     }
 
     override fun onResume() {
         super.onResume()
         Thread.sleep(1000)
     }
+
+    // 개선방법 1) 비동기 호출자로 감싼 동기 함수
+    private fun loadNews() {
+        val headlines = fetchRssHeadlines()
+        val newsCount = findViewById<TextView>(R.id.newsCount)
+        GlobalScope.launch(Dispatchers.Main) {
+            newsCount.text = "Found ${headlines.size} News"
+        }
+    }
+
+    // 개선방법 2) 미리 정의된 디스패처를 갖는 비동기 함수
+    private fun asyncLoadNews() = GlobalScope.launch(netDispatcher) {
+        val headlines = fetchRssHeadlines()
+        val newsCount = findViewById<TextView>(R.id.newsCount)
+        launch(Dispatchers.Main) {
+            newsCount.text = "Found ${headlines.size} News"
+        }
+    }
+
+    // 개선방법 3) 유연한 디스패처를 가지는 비동기 함수
+    private fun asyncLoadNews(dispatcher: CoroutineDispatcher = netDispatcher) =
+        GlobalScope.launch(dispatcher) {
+            val headlines = fetchRssHeadlines()
+            val newsCount = findViewById<TextView>(R.id.newsCount)
+            launch(Dispatchers.Main) {
+                newsCount.text = "Found ${headlines.size} News"
+            }
+        }
 
     private fun fetchRssHeadlines(): List<String> {
         val builder = factory.newDocumentBuilder()
